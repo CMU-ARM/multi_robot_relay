10545229f2aa5cf54806a378715d17f4518296c7
Zhi Tan <zhi.tan@ri.cmu.edu>
2018-09-12 08:41:33 -0400

    Added debug message and code for reconnecting

--- scripts/receiver_relay.py
+++ scripts/receiver_relay.py
@@ -15,23 +15,39 @@ import threading
 
 class ReceiverRelay():
 
-    def __init__(self):
+    def __init__(self, reconnect_on_fail=True):
         #hostname and IP
         self._hostname = rospy.get_param('/receiver/hostname', 'localhost')
         self._port = rospy.get_param('/receiver/port', 9090)
-
-        rospy.loginfo(self._hostname)
 
         self._python_ros = roslibpy.Ros(host=self._hostname, port=self._port)
         self._connected_robot_id = ""
 
 
-        self._python_ros.on_ready(lambda: rospy.loginfo('Connected to ROS on {}:{}'.format(self._hostname, self._port)))
+        self._python_ros.on_ready(lambda: rospy.loginfo('Connected to external ROS on {}:{}'.format(self._hostname, self._port)))
         self._python_ros.on_ready(self.connect,True)       
       
+        self._python_ros.on('close',self._reconnection_protocol)
+        self._restart_notification_flag = False
+        self._keep_alive = reconnect_on_fail
+
         self._multibot_signal_pub = rospy.Publisher('/multibot_local/signal', MultiBotSignal, queue_size=10)
         self._multibot_talking_pub = rospy.Publisher('/multibot_local/talking', MultiBotTalking, queue_size=1)
-        rospy.loginfo('receiver relay online')
+        rospy.loginfo('initialization complete')
+
+    def _reconnection_protocol(self, msg):
+        rospy.loginfo('Receiver lost connection to external ROS master at {}.'.format(self._hostname))
+        
+        #There's a bug where the library doesn't try to reconnect
+        #we just going to hack it by recreating another instance & replacing it
+        #I tried to figure out how to reconnect, but it's not working correctly
+        if self._keep_alive:
+            rospy.loginfo('Attempting reconnect to external ROS master at {}.'.format(self._hostname))
+            self._python_ros = roslibpy.Ros(host=self._hostname, port=self._port)
+            self._python_ros.on_ready(lambda: rospy.loginfo('Reconnected to external ROS on {}:{}'.format(self._hostname, self._port)))
+            self._python_ros.on_ready(self.connect,True)       
+            self._python_ros.on('close',self._reconnection_protocol)
+
 
     def connect(self):
         #listen for the topis that are multibot related
@@ -51,8 +67,9 @@ class ReceiverRelay():
 
 
     def stop(self):
+        self._keep_alive = False
         self._python_ros.terminate()
-        rospy.loginfo('Closing connection to {}:{}'.format(self._hostname, self._port))
+        rospy.loginfo('closing connection to {}:{}'.format(self._hostname, self._port))
 
     def spin(self):
         self._python_ros.run_forever()
@@ -64,7 +81,6 @@ class ReceiverRelay():
 
 
     def _signal_callback(self, msg):
-
         ori_msg = MultiBotSignal()
         ori_msg.header.frame_id = msg['header']['frame_id']
         ori_msg.header.seq = msg['header']['seq']
@@ -74,22 +90,18 @@ class ReceiverRelay():
         ori_msg.signal = msg['signal']
         ori_msg.id = msg['id']
 
-
         #convert back to normal message
         self._multibot_signal_pub.publish(ori_msg)
 
 
 
     def _talking_callback(self, msg):
-        print(msg)
+        rospy.logwarn('TALKING NOT FULLY IMPLEMENTED')
         self._multibot_talking_pub.publish(msg)
 
 def main():
     rospy.init_node('receiver_relay')
     receiver = ReceiverRelay()
-    #receiver = ReceiverRelay(hostname='localhost', port=9090)
-    #receiver = ReceiverRelay(hostname='192.168.0.200', port=9090)
-    #receiver.connect()
 
     def ctrl_handler(sig, frame):
         receiver.stop()
